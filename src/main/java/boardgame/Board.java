e7fe52ec0f260eb5b3048fbf70a192c62cbb6ff9
Daniel Cubo <danielcubo@gmail.com>
2024-12-08 11:45:25 -0300

    BoardException and defensive programming

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -9,6 +9,7 @@ public class Board {
 
     // Constructor with Arguments
     public Board (int rows, int columns) {
+        if(rows < 1 || columns < 1) throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
         this.rows    = rows;
         this.columns = columns;
         this.pieces  = new Piece[rows][columns];
@@ -18,20 +19,31 @@ public class Board {
     public int getRows    () { return this.rows;    }
     public int getColumns () { return this.columns; }
 
-    // Setter Methods
-    public void setRows    (int value) { this.rows    = value; }
-    public void setColumns (int value) { this.columns = value; }
-
     public Piece piece (int row, int column) {
+        if(!this.positionExists(row, column)) throw new BoardException("Position not on the board");
         return this.pieces[row][column];
     }
 
     public Piece piece (Position position) {
+        if(!this.positionExists(position)) throw new BoardException("Position not on the board");
         return this.pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if(this.there_is_a_piece(position)) throw new BoardException("There is already a piece on position "+position);
         this.pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists (int row, int column) {
+        return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
+    }
+    public boolean positionExists (Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean there_is_a_piece (Position position) {
+        if(!this.positionExists(position)) throw new BoardException("Position not on the board");
+        return this.piece(position) != null;
     }
 }
