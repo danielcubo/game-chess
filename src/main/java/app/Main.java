49a397b1fbb6504a7079a531ebd1aeecd6577d1d
Daniel Cubo <danielcubo@gmail.com>
2024-12-08 08:09:20 -0300

    Project created

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 4);
+        System.out.println(pos);
+    }
+}
---
+++ src/main/java/boardgame/Position.java
@@ -0,0 +1,27 @@
+package boardgame;
+
+public class Position {
+
+    // Basic Attributes
+    private int row;
+    private int column;
+
+    // Constructor with Arguments
+    public Position (int row, int column) {
+        this.row    = row;
+        this.column = column;
+    }
+
+    // Getter Methods
+    public int getRow    () { return this.row;    }
+    public int getColumn () { return this.column; }
+
+    // Setter Methods
+    public void setRow    (int value) { this.row = value;    }
+    public void setColumn (int value) { this.column = value; }
+
+    @Override
+    public String toString() {
+        return "Position [row=" + row + ", column=" + column + "]";
+    }
+}
